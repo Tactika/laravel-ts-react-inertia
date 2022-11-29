656ba0dae65b4fed3b14960d813c1732beba0ef1
Damien Richcreek <damien@richcreek.dev>
2022-11-29 12:31:26 -0800

    initial commit

---
+++ resources/ts/Pages/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface WelcomeProps {}
+
+const Welcome: React.FC<WelcomeProps> = (props) => {
+  return (
+      <div>
+        <h1>Welcome Component</h1>
+    </div>
+  )
+};
+export default Welcome;
